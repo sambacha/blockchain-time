e15db07ab96a11fbf765f3f6d51f02eaf7fb007f
sam bacha <sam@freighttrust.com>
2021-02-25 09:00:24 -0800

    Create Constants.js

---
+++ __provider__/Constants.js
@@ -0,0 +1,22 @@
+// The interface names 
+const interfaceName = {
+  Oracle: "Oracle",
+  Registry: "Registry",
+  Store: "Store"
+};
+
+module.exports = {
+  MAX_UINT_VAL: "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+
+  // Max integer that can be safely stored in a vanilla js int.
+  MAX_SAFE_JS_INT: 2147483647,
+
+  // 0x0 contract address
+  ZERO_ADDRESS: "0x0000000000000000000000000000000000000000",
+
+  // Names of different interfaces in the Finder.
+  interfaceName,
+
+  // checkpoint block
+  CHECKPOINT_BLOCK: 10103723
+};
